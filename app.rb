6c578ad9e6224e5a5f54b9b8a2479048201bbc77
ziesop20 <sincerelysophie@hotmail.com>
2018-07-31 18:38:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
  get '/goodbye/:name' do 
-   @person_name = params[:name]
-   "Goodbye, #{person_name}."
+   @name = params[:name]
+   "Goodbye, #{name}."
   end 
 get '/multiply/:num1/:num2' do 
   num1= params[:num1].to_i
